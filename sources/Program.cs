a54a59f8baa4d7f913f9288b8d481b6557cc4ed3
Din0can <alisercoli@gmail.com>
2022-04-26 21:33:33 +0200

    Creation of Random Name Generator. - Implementing Method to Randomize and Set forced length

---
+++ sources/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using darkness.Tools;
+
+NameGenerator Generator = new NameGenerator();
+string name = Generator.GenerateNameRandomLength(3,4);
+Console.WriteLine(name);
---
+++ sources/Tools/NameGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace darkness.Tools
+{
+    public class NameGenerator
+    {
+        public string GenerateName(int length)
+        {
+            char[] selbstlaute = { 'a', 'e', 'i', 'o', 'u' };
+            char[] umlaute = "bcdfghjklmnpqrstvwxyz".ToCharArray();
+
+            string name = "";
+
+            Random selbstlautOderUmlaut = new Random();
+            int decider = selbstlautOderUmlaut.Next(1, 3);
+            for (int counter = 0; counter < length; counter++)
+            {
+                int umlautedecider = selbstlautOderUmlaut.Next(0, umlaute.Length);
+                int selbstlautedecider = selbstlautOderUmlaut.Next(0, selbstlaute.Length);
+
+             
+                    if (decider == 1)
+                    {
+                        char umlautbuchstabe = umlaute[umlautedecider];
+                        name += umlautbuchstabe;
+                        decider = 2;
+                    }
+                    else
+                    {
+                        char selbstlautbuchstabe = selbstlaute[selbstlautedecider];
+                        name += selbstlautbuchstabe;
+                        decider = 1;
+                    }
+            }
+            return name;
+        }
+
+
+        public string GenerateNameRandomLength(int from, int to)
+        {
+            Random selbstlautOderUmlaut = new Random();
+            int length = selbstlautOderUmlaut.Next(from, to +1);
+
+            char[] selbstlaute = { 'a', 'e', 'i', 'o', 'u' };
+            char[] umlaute = "bcdfghjklmnpqrstvwxyz".ToCharArray();
+
+            string name = "";
+
+            int decider = selbstlautOderUmlaut.Next(1, 3);
+            for (int counter = 0; counter < length; counter++)
+            {
+                int umlautedecider = selbstlautOderUmlaut.Next(0, umlaute.Length);
+                int selbstlautedecider = selbstlautOderUmlaut.Next(0, selbstlaute.Length);
+
+
+                if (decider == 1)
+                {
+                    char umlautbuchstabe = umlaute[umlautedecider];
+                    name += umlautbuchstabe;
+                    decider = 2;
+                }
+                else
+                {
+                    char selbstlautbuchstabe = selbstlaute[selbstlautedecider];
+                    name += selbstlautbuchstabe;
+                    decider = 1;
+                }
+            }
+            return name;
+        }
+    }
+
+}
+
